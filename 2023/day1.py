b2db8e3abb188f72a07e71761169d136ed8a093d
Adam Kolb <adam.kolb9@gmail.com>
2023-12-01 21:43:51 -0500

    Day 1 2023 complete

---
+++ 2023/day1.py
@@ -0,0 +1,27 @@
+import csv
+
+file = "day1.csv"
+
+words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+with open(file) as f:
+    data = []
+    csv_reader = csv.reader(f)
+    for line in csv_reader:
+        data.append(line[0])
+
+output = 0
+for line in data:
+    row_data = []
+    for loc, char in enumerate(line):
+        try:
+           row_data.append(int(char))
+        except:
+            for word_value, word in enumerate(words):
+                if word == line[loc:loc + len(word)]:
+                    row_data.append(word_value + 1)
+
+
+    output += row_data[0] * 10 + row_data[-1]
+
+print(output)
